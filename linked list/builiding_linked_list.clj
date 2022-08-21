7cab1370e11614732a93633c3e92631b9e8da3c5
Gabriel Cerqueira <gabrielcmo2018@gmail.com>
2022-08-21 16:43:12 -0300

    *

--- linked list/builiding_linked_list.clj
+++ linked list/builiding_linked_list.clj
@@ -1,4 +1,4 @@
-(ns run.test
+(ns run.linked-list
   (:use [clojure pprint]))
 
 (defn build-node
@@ -29,8 +29,52 @@
     (swap! ll update-tail node)
     ll))
 
+(defn update-head-to-point-to-next-node
+  [ll next-node]
+  (assoc ll :head next-node))
+
+(defn rm-head
+  [ll]
+  (let [dll @ll
+        current-node-head (get dll :head)
+        next-node (get @current-node-head :next)]
+    (swap! ll update-head-to-point-to-next-node next-node)
+    ll))
+
+(defn get-the-previous-node-from-current-tail
+  [node]
+  (let [next-node (get @node :next)
+        next-of-next-node-is-nil? (not (get @next-node :next))]
+    (if next-of-next-node-is-nil?
+      node
+      (recur next-node))))
+
+(defn update-tail-with-previous-node
+  [ll previous-node]
+  (assoc ll :tail previous-node))
+
+
+(defn disconnect-node-to-another
+  [node]
+  (assoc node :next nil))
+
+(defn rm-tail
+  [ll]
+  (let [dll @ll
+        head-node (get dll :head)
+        previous-node-from-tail (get-the-previous-node-from-current-tail head-node)]
+    (swap! ll update-tail-with-previous-node previous-node-from-tail)
+    (swap! previous-node-from-tail disconnect-node-to-another)
+    ll))
+
 (def ll (atom (build-linked-list 1)))
 
 (append! ll 2)
+(append! ll 3)
+(append! ll 4)
+(append! ll 5)
+
+(rm-tail ll)
 
 (pprint ll)
+;(rm-head ll)
