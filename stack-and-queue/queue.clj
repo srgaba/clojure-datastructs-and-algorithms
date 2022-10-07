756314ca0db2866cc65aaa455203952364453e3f
Gabriel Cerqueira <gabrielcmo2018@gmail.com>
2022-10-07 18:45:22 -0300

    dequeue

--- stack-and-queue/queue.clj
+++ stack-and-queue/queue.clj
@@ -24,19 +24,39 @@
 
 (defn assoc-queue! [{:keys [last len] :as queue} new-node]
   (swap! last point-to-next-node new-node)
-  (assoc queue :last new-node :len (+ len 1)))
+  (assoc queue :last new-node :len (inc len)))
 
-(defn enqueue [queue value]
+(defn enqueue! [queue value]
   (let [new-node (atom (build-node value))
         def-queue @queue]
     (if (queue-is-empty? def-queue)
       (swap! queue init-queue new-node)
       (swap! queue assoc-queue! new-node))))
 
+(defn update-first-member [queue second len]
+  (assoc queue :first second :len (dec len)))
+
+(defn has-single-element? [queue]
+  (= (:len queue) 1))
+
+(defn clear-queue! [queue]
+  (assoc queue :first nil :last nil :len 1))
+
+(defn dequeue! [queue]
+  (if (has-single-element? @queue)
+    (swap! queue clear-queue!)
+    (let [def-queue @queue
+          second (:next @(:first def-queue))
+          len (:len def-queue)]
+      (swap! queue update-first-member second len))))
+
 (defn flow []
   (let [queue (atom (build-queue))]
-    (enqueue queue 1)
-    (enqueue queue 2)
+    (enqueue! queue 1)
+    (enqueue! queue 2)
+    (pprint @queue)
+    (dequeue! queue)
+    (dequeue! queue)
     (pprint @queue)))
 
 (flow)
