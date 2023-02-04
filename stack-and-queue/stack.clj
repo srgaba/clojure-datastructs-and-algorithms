189a02b926261b0c0ed8528c8f3b27fb58979a36
gabriel cerqueira <gabriel.moraes@meta.com.br>
2023-02-04 18:15:53 -0300

    fix drop

--- stack-and-queue/stack.clj
+++ stack-and-queue/stack.clj
@@ -9,16 +9,26 @@
   [data stack]
   (swap! stack conj data))
 
+(defn drop!
+  [last-item]
+  (vec (drop-last last-item)))
+
 (defn rm!
   [stack]
-  (swap! stack drop-last))
+  (swap! stack drop!))
 
 (defn init
   []
   (let [stack (build-stack)]
     (add! "foo" stack)
     (add! "bar" stack)
+    (add! "happy" stack)
+    (println @stack)
+    (rm! stack)
+    (println @stack)
     (rm! stack)
     (println @stack)))
 
 (init)
+
+
